a02170f883b17e5191326766aaf9582956621c35
MaheshPatil2004 <patilmaheshwar2004@gmail.com>
2026-08-17 16:23:46 +0530

    fix: move express app to api folder and update rewrites

--- api/index.js
+++ api/index.js
@@ -4,11 +4,11 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-import connectDB from "./utils/db.js";
-import userRoute from "./routes/user.route.js";
-import companyRoute from "./routes/company.route.js";
-import jobRoute from "./routes/job.route.js";
-import applicationRoute from "./routes/application.route.js";
+import connectDB from "../utils/db.js";
+import userRoute from "../routes/user.route.js";
+import companyRoute from "../routes/company.route.js";
+import jobRoute from "../routes/job.route.js";
+import applicationRoute from "../routes/application.route.js";
 
 // Load environment variables
 dotenv.config();
